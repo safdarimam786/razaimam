70e9a0bdfb0fae9ec465e6df6fa23f7afd891ac3
safdarimam786 <safdarimam486@gmail.com>
2026-05-20 17:27:20 +0530

    commit

--- src/hooks/useLenis.js
+++ src/hooks/useLenis.js
@@ -1,23 +1,31 @@
-﻿import { useEffect } from 'react';
+﻿import { useEffect, useRef } from 'react';
 import Lenis from 'lenis';
 
 export function useLenis() {
+  const rafRef = useRef(null);
+
   useEffect(() => {
     const lenis = new Lenis({
-      lerp: 0.08,
-      wheelMultiplier: 0.9,
-      smoothWheel: true
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      lerp: 0.06,
+      wheelMultiplier: 1,
+      smoothWheel: true,
+      smoothTouch: false,
+      touchMultiplier: 1.5,
+      infinite: false,
     });
 
     window.lenis = lenis;
 
-    const raf = (time) => {
+    function raf(time) {
       lenis.raf(time);
-      requestAnimationFrame(raf);
-    };
+      rafRef.current = requestAnimationFrame(raf);
+    }
+    rafRef.current = requestAnimationFrame(raf);
 
-    requestAnimationFrame(raf);
     return () => {
+      if (rafRef.current) cancelAnimationFrame(rafRef.current);
       window.lenis = null;
       lenis.destroy();
     };
